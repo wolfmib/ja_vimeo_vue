d2fa67eb1c68151b661cf94b10dd138c3efd9d2b
Johnny_Wick <wolfmib@gmail.com>
2024-05-06 20:08:14 +0200

    add feature: hello world for embed vimeo video in to main Vue

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <VimeoPlayer videoId="943290462" />
+  </div>
+</template>
+
+<script>
+import VimeoPlayer from './components/VimeoPlayer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    VimeoPlayer
+  }
+};
+</script>
+
